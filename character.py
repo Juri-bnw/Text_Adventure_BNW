4c3795cc3a13d5cbfb776955c113c36d94b3041c
M2005 <marscheil2407@gmail.com>
2026-01-30 14:34:54 +0100

    level system
    level system eingefügt (nur für den spieler9

--- character.py
+++ character.py
@@ -14,14 +14,37 @@ class Character:
         self.charisma = charisma
         self.luck = luck
         self.faehigkeiten = []
-        self.gold = 1000
+        self.gold = 100
         self.inventar = []
+
+    def exp_fuer_naechstes_level(self):
+        return 100 * self.level
+
+    def erhalte_exp(self, menge):
+        self.exp += menge
+        print(f"{self.name} erhält {menge} EP!\n")
+
+        while self.exp >= self.exp_fuer_naechstes_level():
+            self.exp -= self.exp_fuer_naechstes_level()
+            self.level_up()
+
+    def level_up(self):
+        self.level += 1
+        print(f"LEVEL UP! {self.name} ist jetzt Level {self.level}\n")
+
+        self.max_HP += 50
+        self.strength += 5
+        self.defense += 4
+        self.max_mana += 20
+
+        self.current_HP = self.max_HP
+        self.current_mana = self.max_mana
+
 
 Barbar = Character("Barbar", 1, 0, 1000, 100, 70, 10, 70, 200, 20, 20)
 Zauberer = Character("Zauberer", 1, 0, 400, 40, 90, 150, 30, 500, 40, 30)
 Paladin = Character("Paladin", 1, 0, 800, 90, 50, 50, 70, 200, 50, 40)
 Schurke = Character("Schurke", 1, 0, 600, 70, 120, 30, 50, 200, 20, 100)
-
 
 from faehigkeitenbaum import (
     Feuerball, Meteor, Frostnova,
